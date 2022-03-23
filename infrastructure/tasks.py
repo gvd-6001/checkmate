6975818a19bb3cbd8dc44bdcf3ec6fe1d3322a71
Heramb Mishra <amitkumar@geniusvision.in>
2022-03-23 11:36:06 +0530

    checkmate deploy

--- infrastructure/tasks.py
+++ infrastructure/tasks.py
@@ -6,32 +6,27 @@ from infrastructure.models import InfoNVR, PanelInfo
 
 
 @shared_task(bind=True)
-def hikHddStatus(self):
+def hikHddStatus():
     hikNvr = InfoNVR.objects.filter(nvr__selectManufacturer="Hikvision")
     for i in hikNvr:
-        fetchData = Client('http://' + str(i.nvr.ipAddress) + ":" + str(i.nvr.port), i.nvr.username,
-                           i.nvr.password)
+        fetchData = Client('http://' + str(i.nvr.ipAddress) + ":" + str(i.nvr.port), i.nvr.username, i.nvr.password)
         if fetchData:
             deviceResponse = fetchData.System.deviceInfo(method='get')
             hddResponse = fetchData.ContentMgmt.Storage.hdd(method='get')
             InfoNVR.objects.filter(id=i.id).update(status=True)
-            InfoNVR.objects.filter(id=i.id).update(
-                deviceID=deviceResponse["DeviceInfo"]["deviceID"])
-            InfoNVR.objects.filter(id=i.id).update(
-                macAddress=deviceResponse["DeviceInfo"]["macAddress"])
-            InfoNVR.objects.filter(id=i.id).update(
-                modelNo=deviceResponse["DeviceInfo"]["model"])
-            InfoNVR.objects.filter(id=i.id).update(
-                hddCapacity=hddResponse['hddList']['hdd']['capacity'])
-            InfoNVR.objects.filter(id=i.id).update(
-                freeHdd=hddResponse['hddList']['hdd']['freeSpace'])
-            InfoNVR.objects.filter(id=i.id).update(
-                hddType=hddResponse['hddList']['hdd']['hddType'])
+            InfoNVR.objects.filter(id=i.id).update(deviceID=deviceResponse["DeviceInfo"]["deviceID"])
+            InfoNVR.objects.filter(id=i.id).update(macAddress=deviceResponse["DeviceInfo"]["macAddress"])
+            InfoNVR.objects.filter(id=i.id).update(modelNo=deviceResponse["DeviceInfo"]["model"])
+            InfoNVR.objects.filter(id=i.id).update(hddCapacity=hddResponse['hddList']['hdd']['capacity'])
+            InfoNVR.objects.filter(id=i.id).update(freeHdd=hddResponse['hddList']['hdd']['freeSpace'])
+            InfoNVR.objects.filter(id=i.id).update(hddType=hddResponse['hddList']['hdd']['hddType'])
+        else:
+            InfoNVR.objects.filter(id=i.id).update(status=False)
     return "Done"
 
 
 @shared_task(bind=True)
-def vighZoneStatus(self):
+def vighZoneStatus():
     vighPanel = PanelInfo.objects.filter(
         panel__selectManufacturer="Vighanharta")
     for i in vighPanel:
