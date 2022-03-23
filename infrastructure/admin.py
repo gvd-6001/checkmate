e3bd0ed8e00e251a1cd12bb4f73d7865da1ab383
Heramb Mishra <amitkumar@geniusvision.in>
2022-03-23 12:13:49 +0530

    checkmate deploy

--- infrastructure/admin.py
+++ infrastructure/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from . import models
+
+admin.site.register(models.NVR)
+admin.site.register(models.InfoNVR)
+admin.site.register(models.Panel)
+admin.site.register(models.PanelInfo)
